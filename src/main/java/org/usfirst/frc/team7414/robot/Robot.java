fbb9cd95f6c73c20eabdd9cc5507b9de27539a63
sn152 <sn152@pvsd.org>
2019-02-09 13:46:09 -0500

    limit switch prototype

--- src/main/java/org/usfirst/frc/team7414/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team7414/robot/Robot.java
@@ -10,26 +10,18 @@ package org.usfirst.frc.team7414.robot;
 import org.usfirst.frc.team7414.robot.Subsystems.*;
 import org.usfirst.frc.team7414.robot.Monitors.*;
 
-//import java.util.ArrayList;
-//import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
 import org.usfirst.frc.team7414.robot.Hardware.ProximitySensor;
 import org.usfirst.frc.team7414.robot.Monitors.PCMMonitor;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.cameraserver.CameraServer;
-
-import org.opencv.core.Rect;
-import org.opencv.imgproc.Imgproc;
-
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.vision.VisionRunner;
-import edu.wpi.first.wpilibj.vision.VisionPipeline;
-import edu.wpi.first.wpilibj.vision.VisionThread;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 
 public class Robot extends TimedRobot {
 	
@@ -42,38 +34,8 @@ public class Robot extends TimedRobot {
 	public static Compressor compressor;
 	public static ProximitySensor proximity;
 	public static String vision;
-
-	private static final int IMG_WIDTH = 320;
-	private static final int IMG_HEIGHT = 240;
-
-	private VisionThread visionThread;
-	private double centerX = 0.0;
-	private RobotDrive drive;
-
-	private final Object imgLock = new Object();
-	
-	@Override
-	public void robotInit() {
-		compressor = new Compressor(PortMap.compressor);
-		compressor.setClosedLoopControl(true);
-		proximity = new ProximitySensor(PortMap.proximitySensor);
-		//server.getVideo();
-		//server.putVideo(vision, 320, 240);
-		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(PortMap.camera);
-		camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
-		
-		visionThread = new VisionThread(camera, new MyVisionPipeline(), pipeline -> {
-			if (!pipeline.filterContoursOutput().isEmpty()) {
-				Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
-				synchronized (imgLock) {
-					centerX = r.x + (r.width / 2);
-				}
-			}
-		});
-		visionThread.start();
-			
-		drive = new RobotDrive(1, 2);
 	public static PCMMonitor pcmmonitor;
+	DigitalInput HighlimitSwitch, LowlimitSwitch;
 
 	@Override
 	public void robotInit() {
@@ -82,6 +44,8 @@ public class Robot extends TimedRobot {
 		compressor = new Compressor(PortMap.pcm);
 		compressor.setClosedLoopControl(false);
 		proximity = new ProximitySensor(PortMap.proximitySensor);
+		// DigitalInput LowlimitSwitch = new DigitalInput (1);
+		// DigitalInput HighlimitSwitch = new DigitalInput (2);
 		//server.getVideo();
 		//server.putVideo(vision, 320, 240);
 		//pcmmonitor = new PCMMonitor(PortMap.pcm);
@@ -106,14 +70,10 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putString("Range:", Double.toString(proximity.read()));
 		//List<Fault> pcmFaults = pcmmonitor.getFaults();
 		//SmartDashboard.putNumber("Pneumatic faults: ", pcmFaults.size());
-	}
-	@Override
-	public void autonomousPeriodic() {
-    	double centerX;
-    	synchronized (imgLock) {
-        	centerX = this.centerX;
-		}
-    	double turn = centerX - (IMG_WIDTH / 2);
-    	drive.arcadeDrive(-0.6, turn * 0.005);
+		// if (LowlimitSwitch.get()) {
+		// 	//when the lower limit switch is pressed, something happens
+		// } else if (HighlimitSwitch.get()) {
+		// 	//when the upper limit switch is pressed, something else happens
+		// }
 	}
 }
